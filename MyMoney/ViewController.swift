814620cd98f9b564e75d08f27047b0a9345d26b0
Diego Fernando Siqueira Silva <diegofernando.ssiqueira@gmail.com>
2022-06-26 18:16:11 -0300

    Configuração Tela de busca

--- MyMoney/ViewController.swift
+++ MyMoney/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+
+    private lazy var searchController: UISearchController = {
+        let sc = UISearchController(searchResultsController: nil)
+        sc.searchResultsUpdater = self
+        sc.delegate = self
+        sc.obscuresBackgroundDuringPresentation = false
+        sc.searchBar.placeholder = "Buscar"
+        sc.searchBar.autocapitalizationType = .allCharacters
+        return sc
+    } ()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .darkGray
+        self.view.backgroundColor = .white
+        setupNavigationBar()
     }
-
-
+    
+    private func setupNavigationBar() {
+        navigationItem.searchController = searchController
+        
+    }
 }
+
+extension ViewController: UISearchResultsUpdating, UISearchControllerDelegate {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+    
+}
+
 
