8714d0b68bde40f68099609b92a7c982a2cfc385
Diego Fernando Siqueira Silva <diegofernando.ssiqueira@gmail.com>
2022-06-26 13:48:14 -0300

    Projeto Raiz + pods

---
+++ MyMoney/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMoney
+//
+//  Created by Diego Fernando on 26/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .darkGray
+    }
+
+
+}
+
